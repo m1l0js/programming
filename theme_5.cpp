039bdc31bb717e3e9dd53f7cf7820618c1d2eb72
m1l0js <m1l0js@proton.me>
2024-06-20 02:44:23 +0200

    somethign different in the name

--- theme_5.cpp
+++ theme_5.cpp
@@ -3,9 +3,18 @@
 
 int main()
 {
-	std::string name{ "m1l0js"};
-	std::cout << name << '\n';
-	name = "John";
-	std::cout << name << '\n';
+	std::cout << "enter your full name: ";
+	std::string name{};
+	std::getline(std::cin >> std::ws, name);
+
+	std::cout << "enter your car brand: ";
+	std::string car{};
+	std::getline(std::cin >> std::ws, car);
+
+	std::cout << "Your name is " << name << " and the car brand is " << car;
+
+	std::cout << "This is for test";
+	std::string other{};
+	std::getline(std::cin >>  , other);
 	return 0;
 }
