52a2517a90c6ff142698629a623280dd1b2bcce3
m1l0js <m1l0js@proton.me>
2024-06-20 03:24:33 +0200

    New program

--- theme_5.cpp
+++ theme_5.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <string>
+#include <typeinfo>
+
+int getting_age() {
+	int age{};
+	std::cout << "Enter your age: ";
+	std::cin >> age;
+	return age;
+
+}
+
+int getting_length_name()
+{
+	std::cout << "Enter your full name: ";
+	std::string full_name;
+	std::getline(std::cin >> std::ws, full_name);
+	std::cout << typeid(full_name).name() << std::endl;
+	int length{ static_cast<int>(full_name.length()) };
+	std::cout << typeid(length).name() << std::endl;
+	return length;
+
+}
 
 int main()
 {
-	std::cout << "enter your full name: ";
-	std::string name{};
-	std::getline(std::cin >> std::ws, name);
-
-	std::cout << "enter your car brand: ";
-	std::string car{};
-	std::getline(std::cin >> std::ws, car);
-
-	std::cout << "Your name is " << name << " and the car brand is " << car;
-
-	std::cout << "This is for test";
-	std::string other{};
-	std::getline(std::cin >>  , other);
+	int first_value{};
+	int second_value{};
+	first_value = getting_age();
+	second_value = getting_length_name();
+	std::cout << "Your age + length of name is: " << first_value + second_value << '\n';
 	return 0;
 }
